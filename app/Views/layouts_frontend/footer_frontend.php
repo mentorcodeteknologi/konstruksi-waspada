4211b8bdf6f7cdb2261e4dd58169409e28a2c413
CASPER9373 <casperboy9373@gmail.com>
2024-02-10 15:58:45 +0700

    Setup Layout Front End

---
+++ app/Views/layouts_frontend/footer_frontend.php
@@ -0,0 +1,70 @@
+<footer class="footer-light">
+    <section class="section-b-space light-layout">
+        <div class="container">
+            <div class="row footer-theme partition-f">
+                <div class="col-lg-4 col-md-6">
+                    <div class="footer-title footer-mobile-title">
+                        <h4>about</h4>
+                    </div>
+                    <div class="footer-contant">
+                        <div class="footer-logo"><img src="<?= base_url('assets/frontend') ?>/images/icon/logo.png" alt=""></div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                        <div class="footer-social">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fa fa-rss" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col offset-xl-1">
+                    <div class="sub-title">
+                        <div class="footer-title">
+                            <h4>Article Update</h4>
+                        </div>
+                        <div class="footer-contant">
+                            <ul>
+                                <li><a href="#">mens</a></li>
+                                <li><a href="#">womens</a></li>
+                                <li><a href="#">clothing</a></li>
+                                <li><a href="#">accessories</a></li>
+                                <li><a href="#">featured</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="sub-title">
+                        <div class="footer-title">
+                            <h4>Popular Article</h4>
+                        </div>
+                        <div class="footer-contant">
+                            <ul>
+                                <li><a href="#">shipping & return</a></li>
+                                <li><a href="#">secure shopping</a></li>
+                                <li><a href="#">gallary</a></li>
+                                <li><a href="#">affiliates</a></li>
+                                <li><a href="#">contacts</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="sub-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-md-6 col-sm-12">
+                    <div class="footer-end">
+                        <p><i class="fa fa-copyright" aria-hidden="true"></i> 2024 - Article</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
