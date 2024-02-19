a67942d31020cbfce24cebd61c8146a4e76612c4
prayogaep <prayogaerlangga75@gmail.com>
2024-02-19 23:04:45 +0700

    Redesign Auth Form

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -4,23 +4,19 @@
 <!-- breadcrumb start -->
 <div class="breadcrumb-section">
     <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="page-title">
-                            <h2>User Login</h2>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <nav aria-label="breadcrumb" class="theme-breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Home</a></li>
-                                <li class="breadcrumb-item active">login</li>
-                            </ol>
-                        </nav>
-                    </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="page-title">
+                    <h2>User Login</h2>
                 </div>
+            </div>
+            <div class="col-sm-6">
+                <nav aria-label="breadcrumb" class="theme-breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Home</a></li>
+                        <li class="breadcrumb-item active">login</li>
+                    </ol>
+                </nav>
             </div>
         </div>
     </div>
@@ -30,37 +26,33 @@
 
 <?= $this->section('content'); ?>
 <!--section start-->
-<section class="login-page section-b-space">
+<section class="login-page section-b-space bg-white">
     <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <h3>Login</h3>
-                        <div class="theme-card">
-                            <form action="<?= base_url('login') ?>" method="post" class="theme-form" enctype="multipart/form-data">
-                                <?= csrf_field(); ?>
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="text" class="form-control" name="email" id="email" placeholder="Enter your Email" required autofocus>
-                                </div>
-        
-                                <div class="form-group">
-                                    <label for="password">Password</label>
-                                    <input type="password" class="form-control" name="password" id="password" placeholder="Enter your password" required>
-                                </div>
-                                <button type="submit" class="btn btn-solid">Login</button>
-                            </form>
+        <div class="row">
+            <div class="col-lg-6">
+                <h3>Login</h3>
+                <div class="theme-card">
+                    <form action="<?= base_url('login') ?>" method="post" class="theme-form" enctype="multipart/form-data">
+                        <?= csrf_field(); ?>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control" name="email" id="email" placeholder="Enter your Email" required autofocus>
                         </div>
-                    </div>
-                    <div class="col-lg-6 right-login">
-                        <h3>New User</h3>
-                        <div class="theme-card authentication-right">
-                            <h6 class="title-font">Create A Account</h6>
-                            <p>Sign up for a free account at our store. Registration is quick and easy. It allows you to be
-                                able to order from our shop. To start shopping click register.</p><a href="<?= base_url('register') ?>" class="btn btn-solid">Create an Account</a>
+
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Enter your password" required>
                         </div>
-                    </div>
+                        <button type="submit" class="btn btn-solid">Login</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-6 right-login">
+                <h3>New User</h3>
+                <div class="theme-card authentication-right">
+                    <h6 class="title-font">Create A Account</h6>
+                    <p>Sign up for a free account at our store. Registration is quick and easy. It allows you to be
+                        able to order from our shop. To start shopping click register.</p><a href="<?= base_url('register') ?>" class="btn btn-solid">Create an Account</a>
                 </div>
             </div>
         </div>
--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -4,23 +4,19 @@
 <!-- breadcrumb start -->
 <div class="breadcrumb-section">
     <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="page-title">
-                            <h2>create account</h2>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <nav aria-label="breadcrumb" class="theme-breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">create account</li>
-                            </ol>
-                        </nav>
-                    </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="page-title">
+                    <h2>create account</h2>
                 </div>
+            </div>
+            <div class="col-sm-6">
+                <nav aria-label="breadcrumb" class="theme-breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">create account</li>
+                    </ol>
+                </nav>
             </div>
         </div>
     </div>
@@ -38,80 +34,75 @@
 
 <?= $this->section('content'); ?>
 <!--section start-->
-<section class="register-page section-b-space">
+<section class="register-page section-b-space bg-white">
     <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h3>Create Account</h3>
-                        <div class="theme-card">
-                            <form action="<?= base_url('register') ?>" method="post" class="theme-form" enctype="multipart/form-data">
-                                <?= csrf_field(); ?>
+        <div class="row">
+            <div class="col-lg-12">
+                <h3>Create Account</h3>
+                <div class="theme-card">
+                    <form action="<?= base_url('register') ?>" method="post" class="theme-form" enctype="multipart/form-data">
+                        <?= csrf_field(); ?>
 
-                                <div class="form-row row">
-                                    <div class="col-md-6">
-                                        <label for="email">Email</label>
-                                        <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
-                                    </div>
+                        <div class="form-row row">
+                            <div class="col-md-6">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                            </div>
 
-                                    <div class="col-md-6">
-                                        <label for="review">Password</label>
-                                        <input type="password" class="form-control" name="password" id="review" placeholder="Enter your password" required="">
-                                    </div>
-                                </div>
-
-                                <div class="form-row row">
-                                    <div class="col-md-6">
-                                        <label for="nama">Nama</label>
-                                        <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama" required=>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <label>No NIK/NPWP</label>
-                                        <input type="text" name="id_card" id="id_card" class="form-control" placeholder="No NIK/No NPWP " required>
-                                    </div>
-                                </div>
-
-                                <div class="form-row row">
-                                    <div class="col-md-6">
-                                        <label>No Whatsapp</label>
-                                        <input type="text" name="no_hp" id="no_hp" class="form-control" value="+62" placeholder="No Whatsapp" required>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <label>Alamat</label>
-                                        <input type="text" name="alamat" id="alamat" class="form-control" placeholder="Alamat" required>
-                                    </div>
-                                </div>
-
-                                <div class="form-row row">
-                                    <div class="col-md-6">
-                                        <label>Nama Perusahaan</label>
-                                        <input type="text" name="perusahaan" id="perusahaan" class="form-control" placeholder="Perusahaan">
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <label>Jabatan</label>
-                                        <input type="text" name="jabatan" id="jabatan" class="form-control" placeholder="Jabatan">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Foto</label>
-                                    <input type="file" name="foto" id="foto" class="form-control" accept=".jpg,.png,.jpeg">
-                                    <img src="" id="viewImg" class="img-fluid mt-2" width="200px">
-                                </div>
-
-                                <button type="submit" class="btn btn-solid w-auto">create Account</button>
-                            </form>
+                            <div class="col-md-6">
+                                <label for="review">Password</label>
+                                <input type="password" class="form-control" name="password" id="review" placeholder="Enter your password" required="">
+                            </div>
                         </div>
 
-                    </div>
+                        <div class="form-row row">
+                            <div class="col-md-6">
+                                <label for="nama">Nama</label>
+                                <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama" required=>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label>No NIK/NPWP</label>
+                                <input type="text" name="id_card" id="id_card" class="form-control" placeholder="No NIK/No NPWP " required>
+                            </div>
+                        </div>
+
+                        <div class="form-row row">
+                            <div class="col-md-6">
+                                <label>No Whatsapp</label>
+                                <input type="text" name="no_hp" id="no_hp" class="form-control" value="+62" placeholder="No Whatsapp" required>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label>Alamat</label>
+                                <input type="text" name="alamat" id="alamat" class="form-control" placeholder="Alamat" required>
+                            </div>
+                        </div>
+
+                        <div class="form-row row">
+                            <div class="col-md-6">
+                                <label>Nama Perusahaan</label>
+                                <input type="text" name="perusahaan" id="perusahaan" class="form-control" placeholder="Perusahaan">
+                            </div>
+
+                            <div class="col-md-6">
+                                <label>Jabatan</label>
+                                <input type="text" name="jabatan" id="jabatan" class="form-control" placeholder="Jabatan">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Foto</label>
+                            <input type="file" name="foto" id="foto" class="form-control" accept=".jpg,.png,.jpeg">
+                            <img src="" id="viewImg" class="img-fluid mt-2" width="200px">
+                        </div>
+
+                        <button type="submit" class="btn btn-solid w-auto">create Account</button>
+                    </form>
                 </div>
+
             </div>
         </div>
-    </div>
     </div>
 </section>
 <!--Section ends-->
