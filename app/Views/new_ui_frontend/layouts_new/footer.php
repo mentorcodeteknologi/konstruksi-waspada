8d067ceaf5d57c1058d8a08c80286644054eb404
prayogaep <prayogaerlangga75@gmail.com>
2024-07-01 21:57:04 +0700

    footer

--- app/Views/new_ui_frontend/layouts_new/footer.php
+++ app/Views/new_ui_frontend/layouts_new/footer.php
@@ -7,31 +7,38 @@
                     <h4>About</h4>
                 </div>
                 <div>
-                    <p id="total-station-text">Total Station</p>
+                    <p id="total-station-text">Kami siap membantu Anda mewujudkan <br /> 
+                    proyek konstruksi dan pengadaan alat<br />teknik dengan layanan terbaik dari CV.<br />  BNT Bandung</p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6">
                 <div>
-                    <h4>Article Update</h4>
+                    <h4>Kontak</h4>
                 </div>
                 <div>
-                    <ul>
-                        <?php foreach ($footerRecentArtikel as $fra) : ?>
-                            <li><a href="<?= base_url('blog_details/' . $fra['slug']) ?>"><?= $fra['judul'] ?></a></li>
-                        <?php endforeach; ?>
-                    </ul>
+                    <p>
+                    CV. BNT<br />
+                    Komplek Sapta Taruna PU Blok B2 No. 14<br />
+                    Kujangsari, Kec. Bandung Kidul,<br />
+                    Kota Bandung Jawa Barat 40287<br />
+                    Kantor : 022-87521608<br />
+                    Email : bnt.bdg@gmail.com<br />
+                    </p>
                 </div>
+                <!-- <div>
+                    <ul>
+                        <php foreach ($footerRecentArtikel as $fra) : ?>
+                            <li><a href="<= base_url('blog_details/' . $fra['slug']) ?>"><= $fra['judul'] ?></a></li>
+                        <php endforeach; ?>
+                    </ul>
+                </div> -->
             </div>
             <div class="col-lg-4 col-md-6">
                 <div>
-                    <h4>Popular Article</h4>
+                    <h4>Cari Kami di Maps</h4>
                 </div>
                 <div>
-                    <ul>
-                        <?php foreach ($footerPopularArtikel as $fpa) : ?>
-                            <li><a href="<?= base_url('blog_details/' . $fpa['slug']) ?>"><?= $fpa['judul'] ?></a></li>
-                        <?php endforeach; ?>
-                    </ul>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7920.782026617405!2d107.6376458!3d-6.9631159!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e84c77508b43%3A0xd86870b6de788bfc!2sCV%20BNT%20-%20Jual%20Rental%20Service%20%26%20Kalibrasi%20Alat%20Survey%20Total%20Station%20Digital%20Theodolite%20GPS%20RTK%20Topcon%20Sokkia%20Trimble%20Leica!5e0!3m2!1sen!2sid!4v1719845697518!5m2!1sen!2sid" width="450" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
